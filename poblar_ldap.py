e0419445e0bcab46956cf19710efd83f465a3488
Álvaro Beleño <alvarobrod@outlook.es>
2018-11-05 14:07:09 +0100

    Cambios en usuarios.csv, tratamiento de apellidos y uid/gid en poblar_ldap.py

--- poblar_ldap.py
+++ poblar_ldap.py
@@ -1,14 +1,16 @@
-import getpass, ldap3
+import getpass, ldap3, base64
 from ldap3 import Server, Connection, ALL
 
 dom = 'dc=gonzalonazareno,dc=org'
+uid = 2000
+gid = 2000
 
 server = Server('172.22.200.121')
 
 user = input('Usuario: ')
 pwd = getpass.getpass('Contraseña: ')
 
-with open('../usuarios.csv', 'r') as f:
+with open('usuarios.csv', 'r') as f:
 	fichero = f.readlines()
 
 usuarios = []
@@ -20,18 +22,30 @@ for i in fichero:
 try:
 	for i in usuarios:
 		c = Connection(server, 'cn={},{}'.format(user, dom), pwd, auto_bind = True, raise_exceptions = True)
+
 		nombre = i[0]
 		apellidos = i[1]
+
 		if all(ord(char) < 128 for char in nombre):
 			nombre = nombre
 		else:
 			nombre = str(base64.b64encode(nombre.encode())).lstrip("b'").rstrip("'")
+
+		if all(ord(char) < 128 for char in apellidos):
+			apellidos = apellidos
+		else:
+			apellidos = str(base64.b64encode(apellidos.encode())).lstrip("b'").rstrip("'")
+
 		c.add('uid={},ou=People,dc=gonzalonazareno,dc=org'.format(i[3]), \
 		  attributes = {'objectClass': ['top', 'posixAccount', 'inetOrgPerson', 'ldapPublicKey'],\
 		  'givenName': nombre, 'sn': apellidos, 'cn': '{} {}'.format(nombre, apellidos), \
-	 	  'uid': i[3], 'mail': i[2], 'uidNumber': str(2000), 'gidNumber': str(2000), \
+	 	  'uid': i[3], 'mail': i[2], 'uidNumber': str(uid), 'gidNumber': str(gid), \
 	  	  'homeDirectory': '/home/{}'.format(i[3]), 'loginShell': '/bin/bash', \
 	  	  'sshPublicKey': i[4]})
+
 		c.unbind()
+
+		uid += 1
+		gid += 1
 except ldap3.core.exceptions.LDAPInvalidCredentialsResult:
 	print('No se pudo llevar a cabo la conexión: Credenciales incorrectas.')
