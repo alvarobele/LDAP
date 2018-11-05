d1bbd2ac4a3b647e4e743a97cd77a4d16013bd27
Álvaro Beleño <alvarobrod@outlook.es>
2018-11-05 10:55:54 +0100

    Añadido manejo de excepciones cuando las credenciales son incorrectas

--- poblar_ldap.py
+++ poblar_ldap.py
@@ -1,4 +1,4 @@
-import getpass
+import getpass, ldap3
 from ldap3 import Server, Connection, ALL
 
 dom = 'dc=gonzalonazareno,dc=org'
@@ -7,8 +7,6 @@ server = Server('172.22.200.121')
 
 user = input('Usuario: ')
 pwd = getpass.getpass('Contraseña: ')
-
-c = Connection(server, 'cn={},{}'.format(user, dom), pwd, auto_bind = True)
 
 with open('../usuarios.csv', 'r') as f:
 	fichero = f.readlines()
@@ -19,11 +17,15 @@ fichero.pop(0)
 for i in fichero:
 	usuarios.append(i.strip('\n').split(':'))
 
-for i in usuarios:
-	c.add('uid={},ou=People,dc=gonzalonazareno,dc=org'.format(i[3]), \
+try:
+	for i in usuarios:
+		c = Connection(server, 'cn={},{}'.format(user, dom), pwd, auto_bind = True, raise_exceptions = True)
+		c.add('uid={},ou=People,dc=gonzalonazareno,dc=org'.format(i[3]), \
 		  attributes = {'objectClass': ['top', 'posixAccount', 'inetOrgPerson', 'ldapPublicKey'],\
 		  'givenName': i[0], 'sn': i[1], 'cn': '{} {}'.format(i[0], i[1]), \
 	 	  'uid': i[3], 'mail': i[2], 'uidNumber': str(2000), 'gidNumber': str(2000), \
 	  	  'homeDirectory': '/home/{}'.format(i[3]), 'loginShell': '/bin/bash', \
 	  	  'sshPublicKey': i[4]})
-	c.unbind()
+		c.unbind()
+except ldap3.core.exceptions.LDAPInvalidCredentialsResult:
+	print('No se pudo llevar a cabo la conexión: Credenciales incorrectas.')
